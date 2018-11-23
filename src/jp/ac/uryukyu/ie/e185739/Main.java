da94cc961d4748b65097c22b4fc31c2c234caf9f
Choi Jeongho <e185739@ie.u-ryukyu.ac.jp>
2018-11-23 15:50:27 +0900

    ステップ3: APIリファレンスを参照してみよう

--- src/jp/ac/uryukyu/ie/e185739/Main.java
+++ src/jp/ac/uryukyu/ie/e185739/Main.java
@@ -2,12 +2,9 @@ package jp.ac.uryukyu.ie.e185739;
 
 public class Main {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e) {
-            System.out.println(e.toString());
-            System.out.println(e.getMessage());
-        }
+
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+
     }
 }
